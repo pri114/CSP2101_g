f34066b5ceafd4edf431df85bbaf67f99c5c677f
Pri 114 <Pri114@github.com>
2020-04-08 08:28:15 -0700

    Added colour

--- portfolio1/guessage.sh
+++ portfolio1/guessage.sh
@@ -3,44 +3,62 @@
 
 #!/bin/bash
 
-guesscnt=0
-guess=""
-age="$(( $RANDOM % 50 + 21 ))"
+#age="$(( $RANDOM % 50 + 21 ))"      #create a variable "age" that will produce a random number between 20 and 70
+age=35
+guess=""                            #create an empty varibale "guess"
+guesscnt=0                          #set guesscount to zero
 
-#Tell the user what they are required to do
-echo -e "Can you guess my age?\nMy name is Sue. \nI am aged between 20 and 70 years old.\n "
 
-#For as long as the guess does not equal the age ask the user to enter a guess
-while [ "$guess" != "$age" ]; do
-    read -p "How old do you think I am (enter 'q' to quit): " guess
+if ! [ -f "userguess.txt" ]; then   #if a file does not already exist
+    touch userguess.txt             #create an empty file to hold the user's guesses
+else
+    echo "" >| userguess.txt        #otherwise if the file already exists overwrite it with an empty file         
+fi
+
+echo -e "\nCan you guess my age?\nMy name is Sue.\n\
+I am aged between 20 and 70 years old. \n"              
+
+
+while [ "$guess" != "$age" ]; do                                        #For as long as the guess does not equal the age,
+    read -p "How old do you think I am (enter 'q' to quit): " guess     #ask the user to enter a guess or quit the game
+#    echo $guess >> userguess.txt                                       ##########add the guess to the 'userguess.txt' file################
     
-    #if the user enters 'q' than quit the game
-    if [ "$guess" == "q" ]; then 
-        echo 'Goodbye! '
-        break
-    #if the guess is not an integer tell the user their guess is invalid and try again
-    elif ! [[ "$guess" =~ ^[0-9]+$ ]]; then 
-        echo 'Your guess is not a valid number. Try again. '
-    #otherwise if not in range tell the user to try again
-    elif [[ "$guess" -le 20 ]] || [[ "$guess" -ge 70 ]]; then
-        echo 'Your guess is not within range. Try again. '  
-    #otherwise if the guess is valid increase the guess count
-    else
-        ((guesscnt++))
-        
-        #if the guess is correct, congratulate the user
-        if [ "$guess" -eq "$age" ]; then
-            echo "Congratulations! I am $age. You guessed the right age after $guesscnt guesses. "
-            break
-        #if the guess is lower than age, tell the user it's too low
-        elif [ "$guess" -lt "$age" ]; then
-            echo 'Your guess is too low. Try again. '
-        #otherwise if the guess is greater than age, tell the user it's too high 
-        else [ "$guess" -gt "$age" ];
-            echo "Your guess ts too high. Try again. "
+    if [ "$guess" == "q" ]; then                                        #if the user enters 'q' than quit the game and 
+        echo -e "\033[33mGoodbye!\033[0m \n"
+        break                                                           # break out of the loop
+    
+    elif ! [[ "$guess" =~ ^[0-9]+$ ]]; then                                         #if the guess is not an integer
+        echo -e "\033[31mYour guess is not a valid number. Try again.\033[0m\n"     #tell the user their guess is invalid 
+    
+    elif [[ "$guess" -le 20 ]] || [[ "$guess" -ge 70 ]]; then                       #otherwise if the guess is not between 20 and 70 
+        echo -e "\033[31mYour guess is not within range.\033[0m Try again. \n"      #tell the user their guess is not in range       
+      
+    #elif [ grep -q '$guess' userguess.txt ]; then                      #########check the 'userguess.txt' file for duplicated user input###########
+    #    echo -e "You've already tried that number. Try again. \n"      #########tell the user if their guess has already been tried###############
+     
+    else                                                                
+        ((guesscnt++))                      #otherwise if the guess is valid increase the guess count
+        if [ $guesscnt -eq 1 ]; then        #if the guess count is equal to 1 use singular word 'guess' in the variable singplu
+            singplu=guess               
+        else
+            singplu=guesses                 #otherwise use the plural word 'guesses' in the variable singplu
+        fi
+
+        if [ "$guess" -eq "$age" ]; then                            #if the guess is correct congratulate the user  
+            echo -e "\033[33mCongratulations!\033[0m I am $age."\
+            "You guessed the right age in $guesscnt $singplu. \n"   #tell the user how many attempts it took
+            #cat "userguess.txt"                                    #######REMOVE#####################                   
+            break                                                   #break out of the loop
+
+        elif [ "$guess" -lt "$age" ]; then                          #if the guess is lower than age, 
+            echo -e "Your guess is too low. Try again. \n"          #tell the user it's too low            
+
+        else [ "$guess" -gt "$age" ];                               #otherwise if the guess is greater than age, 
+            echo -e "Your guess is too high. Try again. \n"         #tell the user it's too high
         
         fi
     
-    fi
+    fi  
+
 done
 
