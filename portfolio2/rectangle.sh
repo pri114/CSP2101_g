7f6c16c5973eda3f54a926cd2dab5ccf06ca25fa
Pri 114 <Pri114@github.com>
2020-05-21 09:14:13 -0700

    last update

--- portfolio2/rectangle.sh
+++ portfolio2/rectangle.sh
@@ -7,38 +7,39 @@ RED='\033[00;31m'	    # Use ${RED} in the code
 BLUE='\033[00;34m'	    # Use ${BLUE} in the code
 NC='\033[00;0m'		    # Use ${NC} in the code
 
-# Prompt the user to enter the text file to be formatted
+# Prompt the user to enter the text file to be formatted.
 read -p 'Enter the filename to be formatted: ' selfile
 
-# If the file does not exist let the user know and exit 
+# If the file does not exist let the user know and exit.
 if ! [ -f $selfile ]; then 
     echo -e "${RED}Error: ${NC}The file '$selfile' does not exist.\n"
     exit 0
 
 # Otherwise if the file exists
 else 
-    # check it's a text file and search the file to ensure it's in the right format
+    # check it's a text file and search the file contents to ensure it's in the right format.
     if [[ $selfile == *.txt ]]; then 
         grep -q -E 'Rec[0-9]+\,[0-9]+\,[0-9]+\,[0-9]+\,+[a-zA-Z]' $selfile  
-        if [ $? -eq 0 ]; then                  # If true the text file can be formatted
+        if [ $? -eq 0 ]; then                  # If true the text file can be formatted.
             echo -e "\nFormatting...\n"             
-            formfile="${selfile%.txt}_f.txt"   # create a new file and store in the variable 'formfile'
+            formfile="${selfile%.txt}_f.txt"   # Create a new file and store in the variable 'formfile'.
 
             # Parse out the lines from the selected file to sed
             # Delete the first line
-            # Find every instance of the word 'Rec' gloablly and add the word 'Name: ' before it
-            # Read every line, locate the first comma in each line and substitute with a tab and a heading. 
-                # i.e. the first comma in each line will be replaced with a tab and the word 'Height'
-                # the following comma (now the first) will be replaced with a tab and the word 'Width' and so forth 
-                # until all four commas in each line have been replaced.
+            # Find every instance of the word 'Rec' and add the word 'Name: ' before it
+            # Locate the first comma in each line and substitute with a tab and a heading
+                # i.e. the first comma in each line will be replaced with a tab and the word 'Height:'
+                # the following comma (now the first) will be replaced with a tab and the word 'Width:' 
+                # and so forth until all four commas in each line have been replaced.
                 sed -e '1d'\
                 -e 's/Rec/Name: &/g'\
                 -e 's/,/\tHeight: /'\
                 -e 's/,/\tWidth: /'\
                 -e 's/,/\tArea: /'\
-                -e 's/,/\tColour: /' $selfile > $formfile # Write the formatted output to the newly created file stored in the variable 'formfile'
+                -e 's/,/\tColour: /' $selfile > $formfile 
+                # Write the formatted output to the newly created file stored in the variable 'formfile'.
 
-        # If the file is not the right format let the user know and exit
+        # If the file is not in the right format let the user know and exit.
         else    
             echo "${RED}Error: ${NC}The file $selfile cannot be formatted.\n"
             exit 0
@@ -52,8 +53,8 @@ else
 
 fi
 
-# Print contents of the output file formatted into a table with column titles in blue for readablity
-# This is done by substituting each respective heading globally with the same heading printed in blue
+# Print contents of the output file formatted into a table with column titles in blue for readablity.
+# This is done by substituting each respective heading globally with the same heading printed in blue.
 cat $formfile | sed -e "s/Name:/`printf "${BLUE}Name:${NC}"`/g"\
                     -e "s/Height:/`printf "${BLUE}Height:${NC}"`/g"\
                     -e "s/Width:/`printf "${BLUE}Width:${NC}"`/g"\
